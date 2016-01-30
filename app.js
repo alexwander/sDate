441a5d12d01732ecb87676fa749409b682415a02
alexwander <ryndyuk.alexander@gmail.com>
2016-01-31 03:17:04 +0630

    edit gr

--- app.js
+++ app.js
@@ -160,6 +160,26 @@ app.config(['$routeProvider',
           }
         })
 
+        .when('/:groupId/edit-group',{
+          templateUrl: 'views/edit-group.html',
+          controller:'EditGroupCtrl',
+          resolve: {
+            user: function($rootScope, $firebase, $firebaseSimpleLogin){
+              if(!$rootScope.userID){
+                $rootScope.loading = 1;
+                var ref = new Firebase('https://fitnesskdm.firebaseIO.com');
+                var auth = $firebaseSimpleLogin(ref);
+                return auth.$getCurrentUser()
+              }else{
+                return;
+              }
+            }
+
+          }
+        })
+
+
+
 
 
 
